21d2fef0ce4b581132ed27fd339a3bc9bfa2541f
Milian Wolff <mail@milianw.de>
2011-02-09 16:42:05 +0100

    put params to title of plot

--- plot_folder.sh
+++ plot_folder.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "plotting files from: $1"
 cat $1/* | sort -g > /tmp/.plt
-echo "$GNUPLOT_CMD set grid; plot '/tmp/.plt' w lines" | gnuplot -persist
+t=$(basename $1)
+echo "$GNUPLOT_CMD set grid; plot '/tmp/.plt' w lines title '$t'" | gnuplot -persist
