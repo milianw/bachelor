006812640d3f9d39bcc66a1be613f3dc4b2dfe01
Milian Wolff <mail@milianw.de>
2011-03-08 14:24:19 +0100

    use float and add note that it only works in Rel* or Release mode, we need that anyways so default to that

--- spinlib/fptype.h
+++ spinlib/fptype.h
@@ -24,9 +24,10 @@
 
 #include <complex>
 
-typedef double fp;
+// typedef double fp;
 // typedef long double fp;
-// typedef float fp;
+///NOTE: float only seems to work in RelWithDebInfo or Release mode...
+typedef float fp;
 
 typedef std::complex<fp> c_fp;
 
