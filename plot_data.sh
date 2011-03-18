4ba5ca817bcabae389c777ac917c91a81e51f6d0
Milian Wolff <mail@milianw.de>
2011-03-18 19:14:25 +0100

    put plot data into files depending on data file name, making it simpler to compare data

--- plot_data.sh
+++ plot_data.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 echo "plotting files from: $1"
 
+plt_base=/tmp/.plt_$(basename $1)_
+
 if [ -d $1 ]; then
-  cat $1/* | sort -g > /tmp/.plt
+  cat $1/* | sort -g > ${plt_base}raw
 else
-  cat $1 | sort -g > /tmp/.plt
+  cat $1 | sort -g > ${plt_base}raw
 fi
 
-./convolute/convolute $1 > /tmp/.plt_conv
+./convolute/convolute $1 > ${plt_base}conv
 a=($(basename $1 | tr ":" "\n"))
 t="Spectrum for ${a[0]} 1H, ${a[1]} 14H, mwFreq = ${a[4]} GHz, B-Range: ${a[2]} (${a[3]} steps) ${a[5]}"
 echo "$GNUPLOT_CMD set title '$t';
       set xlabel 'static B-field in Tesla';
       set ylabel 'intensity in a.u.';
       set grid;
-      plot '/tmp/.plt_conv' w lines notitle, '/tmp/.plt' w impulses notitle" | gnuplot -persist
+      plot '${plt_base}conv' w lines notitle, '${plt_base}raw' w impulses notitle" | gnuplot -persist
