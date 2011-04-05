50900c64904e50f409345adfea319e968d832d1d
Milian Wolff <mail@milianw.de>
2011-04-05 14:20:00 +0200

    improve easy spin runner

--- easy_runner.sh
+++ easy_runner.sh
@@ -5,4 +5,18 @@ f="/tmp/$t.easy.dat";
 echo "saveSpec(B, Spec, '$f');" >> /tmp/test.m
 echo "exit;" >> /tmp/test.m
 matlab -nojvm -nosplash -r "run('/tmp/test.m');" || exit
-echo "set title '$t (easyspin)'; plot '$f' w lines notitle;" | gnuplot -persist
+
+echo "plotting files from: $f"
+
+CONVOLUTE_WIDTH=1 ./convolute/convolute $f > ${f}.conv
+
+t="Spectrum for $f, mwFreq = 9.5 GHz (EasySpin)"
+pltraw=", '$f' w impulses notitle";
+if [[ "$NOPLOT_RAW" == "1" ]]; then
+  pltraw=""
+fi
+echo "$GNUPLOT_CMD set title '$t';
+      set xlabel 'static B-field in Tesla';
+      set ylabel 'intensity in a.u.';
+      set grid;
+      plot '${f}.conv' w lines notitle$pltraw" | gnuplot -persist
