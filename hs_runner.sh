ee0eb276c7133d3069db92963db18ff9749a5057
Milian Wolff <mail@milianw.de>
2011-04-11 12:59:53 +0200

    make it possible to pass micro wave frequency to hs_runner

--- hs_runner.sh
+++ hs_runner.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-../run_and_plot_intensity.sh ./non-mpi/hs -s $1 -i 0-0:auto:9.5
+if [[ "$2" != "" ]]; then
+  mw=$2
+else
+  mw=9.5
+fi
+../run_and_plot_intensity.sh ./non-mpi/hs -s $1 -i 0-0:auto:$mw
