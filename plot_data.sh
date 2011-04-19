cd264c854a7d74eb9ab82070fddbdcbe438ff957
Milian Wolff <mail@milianw.de>
2011-04-19 19:14:49 +0200

    make convoluted plot better descernible, bring back option to disable plotting of raw data

--- plot_data.sh
+++ plot_data.sh
@@ -30,9 +30,15 @@ fi
 
 t="Spectrum for ${a[2]} 1H, ${a[3]} 14H, mwFreq = ${a[4]} GHz\\nB-Range: ${a[0]} ($steps steps) ${a[5]}$TITLE_ADD"
 
+pltraw="'${plt_base}conv' using 1:(\$3 == 1 ? \$2 : 1/0) w impulses notitle,";
+
+if [[ "$PLOT_NO_RAW" != "" ]]; then
+  pltraw=""
+fi
+
 echo "$GNUPLOT_CMD set title \"$t\";
       set xlabel 'static B-field in Tesla';
       set ylabel 'intensity in a.u.';
       set grid;
-      plot '${plt_base}conv' using 1:(\$3 == 0 ? \$2 : 1/0) w lines notitle, \\
-      '${plt_base}conv' using 1:(\$3 == 1 ? \$2 : 1/0) w impulses notitle" | gnuplot -persist
+      plot $pltraw \\
+      '${plt_base}conv' using 1:(\$3 == 0 ? \$2 : 1/0) w lines notitle lt 7" | gnuplot -persist
