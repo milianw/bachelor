82d0ce195175b697389bd5ccaeeeafff220ca1db
John Paul Adrian Glaubitz <glaubitz@physik.fu-berlin.de>
2012-06-06 14:44:34 +0200

    More boost filesystem API fixes.

--- mpi/mpi_master.cpp
+++ mpi/mpi_master.cpp
@@ -84,9 +84,9 @@ void MPIMaster::calculateIntensity(const fp from, const fp to, const int steps, 
   fs::directory_iterator end;
   while(it != end) {
     if (fs::is_regular_file(it->status())) {
-      if (boost::ends_with(it->path().filename(), ".job")) {
+      if (it->path().extension() == "job") {
         m_lastJobId = max(boost::lexical_cast<unsigned int>(it->path().stem()), m_lastJobId);
-        bool added = readdJob(it->path().file_string());
+        bool added = readdJob(it->path().string());
         continuingJobs = continuingJobs || added;
       }
     }
