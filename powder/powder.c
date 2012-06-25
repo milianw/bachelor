0579a02b63a910ed68ce6f5eb3ef244cefd86913
John Paul Adrian Glaubitz <glaubitz@physik.fu-berlin.de>
2012-06-25 15:02:59 +0200

    Check for intensities to be summed to be valid numbers in create_regular_grid, add zero otherwise.

--- powder/powder.c
+++ powder/powder.c
@@ -222,8 +222,10 @@ int create_regular_grid (epr_spectrum * spectrum, double accuracy) {
 
       for (i = 0; i < spectrum->size; i++) {
       	j = (int) floor ((spectrum->B[i][0] - B_min) / accuracy);
-      	new_spectrum.I[j][0] += spectrum->I[i][0] * spectrum->O[i].weight;
-      	new_spectrum.I[j][1] += spectrum->I[i][1] * spectrum->O[i].weight;
+      	if (!isnan(spectrum->I[i][0]))
+	  new_spectrum.I[j][0] += spectrum->I[i][0] * spectrum->O[i].weight;
+	if (!isnan(spectrum->I[i][1]))
+	  new_spectrum.I[j][1] += spectrum->I[i][1] * spectrum->O[i].weight;
       }
     }
 
