d1620d922df8890b7e35c86af21e13dbb16c48a1
John Paul Adrian Glaubitz <glaubitz@physik.fu-berlin.de>
2012-07-16 19:28:01 +0200

    Forgot to increment counter for line parsing in average_multiple_spectra.

--- powder/powder.c
+++ powder/powder.c
@@ -337,6 +337,7 @@ int average_multiple_spectra(FILE ** input_spectra_files, int spectrum_count, ep
       if (sscanf(buffer, "%lg %lg %lg %lg %lg %lg[^\n]\n", &B, &I, &O.x, &O.y, &O.z, &O.weight) == 6) {
 	averaged_spectrum->B[j][0] = B;
 	averaged_spectrum->I[j][0] = (i == 0) ? I : (I + averaged_spectrum->I[j][0]);
+	j++;
       }
     }
   }
